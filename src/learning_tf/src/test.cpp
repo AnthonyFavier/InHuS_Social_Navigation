db5a0e3aab8211807d11b82012197534ffe591de
AnthonyFavier <anthony.favier@laas.fr>
2020-09-29 14:01:12 +0200

    package learning_tf and navigation

---
+++ src/learning_tf/src/test.cpp
@@ -0,0 +1,30 @@
+#include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+
+int main(int argc, char** argv)
+{
+	ros::init(argc, argv, "my_tf_broadcaster");
+
+	ros::NodeHandle node;
+
+	ros::Rate rate(50);
+
+
+	while(ros::ok())
+	{
+		static tf::TransformBroadcaster br;
+		tf::Transform transform;
+		transform.setOrigin( tf::Vector3(0, 0, 0.0) );
+		tf::Quaternion q;
+		q.setRPY(0, 0, 0);
+		transform.setRotation(q);
+		br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "base_footprint", "base_laser_link"));
+//		br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "base_footprint", "base_link"));
+		br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "map", "odom"));
+
+		ros::spinOnce();
+		rate.sleep();
+	}
+
+	return 0;
+};
