57fb52393dc77dd0b9428df7239cd5994056e0bf
AnthonyFavier <anthony.favier@laas.fr>
2020-09-29 14:32:56 +0200

    controller + rename logManager_node.cpp

---
+++ src/human_sim/src/controller_node.cpp
@@ -0,0 +1,29 @@
+#include "ros/ros.h"
+#include "geometry_msgs/Twist.h"
+
+ros::Publisher pub_cmd_vel;
+
+void teleopCmdCallback(const geometry_msgs::Twist::ConstPtr& msg)
+{
+
+}
+
+void perturbatedCmdCallback(const geometry_msgs::Twist::ConstPtr& msg)
+{
+	pub_cmd_vel.publish(*msg);
+}
+
+int main(int argc, char** argv)
+{
+	ros::init(argc, argv, "controller");
+	ros::NodeHandle nh;
+
+	ros::Subscriber sub_teleop = 		nh.subscribe("/controller/teleop_cmd", 100, teleopCmdCallback);
+	ros::Subscriber sub_perturbated_cmd = 	nh.subscribe("/controller/perturbated_cmd", 100, perturbatedCmdCallback);
+
+	pub_cmd_vel = 	nh.advertise<geometry_msgs::Twist>("/cmd_vel", 100);
+
+	ros::spin();
+
+	return 0;
+}
