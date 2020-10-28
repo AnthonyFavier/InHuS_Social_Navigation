c72a8188b4e105fd155f5dc717d3ba75357403f7
AnthonyFavier <anthony.favier@laas.fr>
2020-10-28 15:30:44 +0100

    corridor tests poses

--- src/morse_ros/scenarios/laas_adream_scenario.py
+++ src/morse_ros/scenarios/laas_adream_scenario.py
@@ -3,12 +3,14 @@ from morse.builder import *
 # set the environment to laas_adream
 env = Environment("laas_adream.blend", fastmode=False)
 env.set_camera_location([18.0, 4.0, 10.0])
-env.set_camera_rotation([1.0, 0.0 , 1.57])
+env.set_camera_rotation([0.9, 0.0 , 1.57])
 
 # creates a new instance of the morsy robot
 human = Human()
 human.properties(GroundRobot = True)
-human.translate(x=2, y=2, z=0);
+#human.translate(x=2, y=2, z=0); #start
+human.translate(x=10.1, y=2.6, z=0); #corridor
+human.rotate(0,0,1.57)
 
 motionH = MotionXYW()
 motionH.properties(ControlType = 'Position')
@@ -23,8 +25,10 @@ pose.add_interface('ros', topic="morse/human_pose")
 
 pr2 = BarePR2()
 pr2.properties(GroundRobot = True)
-#pr2.translate(5.0, 2.0, 0.0)
-pr2.translate(5.8, 4.6, 0.0)
+#pr2.translate(5.0, 2.0, 0.0) #start
+#pr2.translate(5.8, 4.6, 0.0) #block appart
+pr2.translate(10, 13.5, 0.0) #corridor
+pr2.rotate(0,0,-1.57)
 
 keyboard = Keyboard()
 pr2.append(keyboard)
