ef1abe48b7d4c8256c19dc10230fdcc14427a33a
Anthony <anthony.favier@laas.fr>
2020-12-22 15:14:22 +0100

    pulling.sh

---
+++ src/pulling.sh
@@ -0,0 +1,3 @@
+git clone -b melodic-devel https://github.com/AnthonyFavier/navigation
+git clone -b melodic_devl https://github.com/sphanit/hanp_msgs
+git clone -b master https://github.com/AnthonyFavier/hanp_prediction
