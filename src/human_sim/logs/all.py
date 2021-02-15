dd36656735e946a4fb3b9f3c7d2d74b6ccd95c5f
Anthony <anthony.favier@laas.fr>
2021-02-15 14:48:53 +0100

    all way better

--- src/human_sim/logs/all.py
+++ src/human_sim/logs/all.py
@@ -1,26 +1,13 @@
-import sys, os, threading
+import sys, os
 
-def graph_thread():
+if len(sys.argv)>=3:
+    os.system("python path_color.py poseLog.txt B " + sys.argv[1] + " " + sys.argv[2])
+    os.system("python graph.py log.txt " + sys.argv[1] + " " + sys.argv[2])
+elif len(sys.argv)>=2:
+    os.system("python path_color.py poseLog.txt B " + sys.argv[1])
+    os.system("python graph.py log.txt " + sys.argv[1])
+else:
+    os.system("python path_color.py poseLog.txt B")
+    os.system("python graph.py log.txt")
 
-    if len(sys.argv)>=3:
-        os.system("python graph.py log.txt " + sys.argv[1] + " " + sys.argv[2])
-    elif len(sys.argv)>=2:
-        os.system("python graph.py log.txt " + sys.argv[1])
-    else:
-        os.system("python graph.py log.txt")
-
-def path_color_thread():
-
-    if len(sys.argv)>=3:
-        os.system("python path_color.py poseLog.txt B " + sys.argv[1] + " " + sys.argv[2])
-    elif len(sys.argv)>=2:
-        os.system("python path_color.py poseLog.txt B " + sys.argv[1])
-    else:
-        os.system("python path_color.py poseLog.txt B")
-
-if __name__ == "__main__":
-
-    x = threading.Thread(target=graph_thread)
-    x.start()
-    path_color_thread()
     
