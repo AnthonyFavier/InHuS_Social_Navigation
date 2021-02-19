d8426aea8db98974d4bea3cbbda1c079939baa36
Anthony <anthony.favier@laas.fr>
2021-02-19 16:09:53 +0100

    rm expe => specific repo

--- experiences/play.sh
+++
@@ -1,1 +0,0 @@
-rosbag play $1 /morse/human_pose:=/bag_pose_h /morse/robot_pose:=/bag_pose_r
--- experiences/record.sh
+++
@@ -1,1 +0,0 @@
-rosbag record /morse/human_pose /morse/robot_pose /human/new_goal /move_base_simple/goal -O $1
