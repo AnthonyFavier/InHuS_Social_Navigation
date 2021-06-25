93c5ed1efe74088b8b5c9c42115dbd399c5ec262
AnthonyFavier <anthony.favier@laas.fr>
2021-06-25 12:54:20 +0200

    Merge logManager and PoseLog

--- src/inhus_system/inhus/include/logManager.h
+++ src/inhus_system/inhus/include/logManager.h
@@ -5,6 +5,7 @@
 #include <ros/package.h>
 #include "std_msgs/String.h"
 #include "geometry_msgs/Twist.h"
+#include "geometry_msgs/Pose2D.h"
 #include <fstream>
 #include <math.h>
 
@@ -15,16 +16,26 @@ class LogManager
 public:
 	LogManager();
 	~LogManager();
+
 	void logCallback(const std_msgs::String::ConstPtr& msg);
 	void velHCallback(const geometry_msgs::Twist::ConstPtr& msg);
 	void velRCallback(const geometry_msgs::Twist::ConstPtr& msg);
+
+	void poseHCallback(const geometry_msgs::Pose2D::ConstPtr& msg);
+	void poseRCallback(const geometry_msgs::Pose2D::ConstPtr& msg);
+
 private:
 	ros::NodeHandle nh_;
+	string path_;
+
+	ofstream log_file_inhus_;
 	ros::Subscriber sub_log_;
 	ros::Subscriber sub_vel_H_;
 	ros::Subscriber sub_vel_R_;
-	ofstream log_file_;
-	string path_;
+
+	ofstream log_file_inhus_poses_;
+	ros::Subscriber sub_pose_H_;
+	ros::Subscriber sub_pose_R_;
 };
 
 #endif
--- src/inhus_system/inhus/src/logManager_node.cpp
+++ src/inhus_system/inhus/src/logManager_node.cpp
@@ -5,33 +5,51 @@
 LogManager::LogManager()
 {
 	path_ = ros::package::getPath("inhus");
-	log_file_.open(path_ + "/logs/log_data/log.txt");
-	log_file_ << "LOG STARTS : " << ros::Time::now() << endl;
 
+	log_file_inhus_.open(path_ + "/logs/log_data/log.txt");
+	log_file_inhus_ << "LOG STARTS : " << ros::Time::now() << endl;
 	sub_log_ = nh_.subscribe("log", 100, &LogManager::logCallback, this);
 	sub_vel_H_ = nh_.subscribe("known/human_vel", 100, &LogManager::velHCallback, this);
 	sub_vel_R_ = nh_.subscribe("known/robot_vel", 100, &LogManager::velRCallback, this);
+
+	log_file_inhus_poses_.open(path_ + "/logs/log_data/poseLog.txt");
+	log_file_inhus_poses_ << "LOG STARTS : " << ros::Time::now() << endl;
+	sub_pose_H_ = nh_.subscribe("interface/in/human_pose", 100, &LogManager::poseHCallback, this);
+	sub_pose_R_ = nh_.subscribe("interface/in/robot_pose", 100, &LogManager::poseRCallback, this);
 }
 
 LogManager::~LogManager()
 {
-	log_file_.close();
+	log_file_inhus_.close();
+	log_file_inhus_poses_.close();
 }
 
+////////////////////// LOG_FILE_INHUS ///////////////////////
 void LogManager::logCallback(const std_msgs::String::ConstPtr& msg)
 {
-	log_file_ << ros::Time::now() << " : " <<  msg->data << endl;
+	log_file_inhus_ << ros::Time::now() << " : " <<  msg->data << endl;
 }
 
 void LogManager::velHCallback(const geometry_msgs::Twist::ConstPtr& msg)
 {
 
-	log_file_ << ros::Time::now() << " : LOG VEL_H " << std::to_string(sqrt(pow(msg->linear.x,2) + pow(msg->linear.y,2))) << endl;
+	log_file_inhus_ << ros::Time::now() << " : LOG VEL_H " << std::to_string(sqrt(pow(msg->linear.x,2) + pow(msg->linear.y,2))) << endl;
 }
 
 void LogManager::velRCallback(const geometry_msgs::Twist::ConstPtr& msg)
 {
-	log_file_ << ros::Time::now() << " : LOG VEL_R " << std::to_string(sqrt(pow(msg->linear.x,2) + pow(msg->linear.y,2))) << endl;
+	log_file_inhus_ << ros::Time::now() << " : LOG VEL_R " << std::to_string(sqrt(pow(msg->linear.x,2) + pow(msg->linear.y,2))) << endl;
+}
+
+///////////////// LOG_FILE_INHUS_POSES /////////////////////
+void LogManager::poseHCallback(const geometry_msgs::Pose2D::ConstPtr& msg)
+{
+	log_file_inhus_poses_ << ros::Time::now() << " : H " << std::to_string(ros::Time::now().toSec()) << " " << msg->x << " " << msg->y << endl;
+}
+
+void LogManager::poseRCallback(const geometry_msgs::Pose2D::ConstPtr& msg)
+{
+	log_file_inhus_poses_ << ros::Time::now() << " : R " << std::to_string(ros::Time::now().toSec()) << " " << msg->x << " " << msg->y << endl;
 }
 
 //////////////////////// MAIN ///////////////////////////////
