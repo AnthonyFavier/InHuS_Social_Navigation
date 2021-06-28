e6b353b2382cab719fa9a17b5741bc0867443f2e
AnthonyFavier <anthony.favier@laas.fr>
2021-06-28 10:41:13 +0200

    logManagerCohan only R for now

--- src/inhus_system/inhus_navigation/include/logManagerCohan.h
+++ src/inhus_system/inhus_navigation/include/logManagerCohan.h
@@ -5,7 +5,9 @@
 #include <ros/package.h>
 #include "std_msgs/String.h"
 #include "geometry_msgs/Twist.h"
+#include "geometry_msgs/PoseStamped.h"
 #include "nav_msgs/Odometry.h"
+#include "actionlib_msgs/GoalStatusArray.h"
 #include <fstream>
 #include <math.h>
 
@@ -18,15 +20,30 @@ public:
 	~LogManagerCohan();
 
 	void logCallback(const std_msgs::String::ConstPtr& msg);
-	void poseRCallback(const nav_msgs::Odometry::ConstPtr& msg);
+	void odomRCB(const nav_msgs::Odometry::ConstPtr& msg);
+	void odomHCB(const nav_msgs::Odometry::ConstPtr& msg);
+	void robotGoalCB(const geometry_msgs::PoseStamped::ConstPtr& msg);
+	void robotGoalStatusCB(const actionlib_msgs::GoalStatusArray::ConstPtr& msg);
 
 private:
 	ros::NodeHandle nh_;
 	string path_;
 
+	nav_msgs::Odometry odom_h_;
+	nav_msgs::Odometry odom_r_;
+
+	bool goal_received_;
+	bool goal_done_;
+
+	int id_;
+
 	ofstream log_file_;
 	ros::Subscriber sub_log_;
-	ros::Subscriber sub_pose_R_;
+	ros::Subscriber sub_odom_R_;
+	ros::Subscriber sub_odom_H_;
+
+	ros::Subscriber sub_robot_goal_status_;
+	ros::Subscriber sub_robot_goal_;
 };
 
 #endif
--- src/inhus_system/inhus_navigation/src/logManagerCohan_node.cpp
+++ src/inhus_system/inhus_navigation/src/logManagerCohan_node.cpp
@@ -6,9 +6,15 @@ LogManagerCohan::LogManagerCohan()
 {
 	path_ = ros::package::getPath("inhus");
 
-	log_file_.open(path_ + "/logs/log_data/log_cohan.txt");
-	sub_log_ = nh_.subscribe("/move_base/HATebLocalPlannerROS/hateb_log", 100, &LogManagerCohan::logCallback, this);
-	sub_pose_R_ = nh_.subscribe("/odom", 100, &LogManagerCohan::poseRCallback, this);
+	id_ = 0;
+
+	sub_odom_R_ = nh_.subscribe("/odom", 100, &LogManagerCohan::odomRCB, this);
+	sub_odom_H_ = nh_.subscribe("/human1/odom", 100, &LogManagerCohan::odomHCB, this);
+	sub_robot_goal_ = nh_.subscribe("/robot_goal", 100, &LogManagerCohan::robotGoalCB, this);
+	sub_robot_goal_status_ = nh_.subscribe("/robot_goal_status", 100, &LogManagerCohan::robotGoalStatusCB, this);
+
+	goal_received_ = false;
+	goal_done_ = false;
 }
 
 LogManagerCohan::~LogManagerCohan()
@@ -16,22 +22,59 @@ LogManagerCohan::~LogManagerCohan()
 	log_file_.close();
 }
 
-void LogManagerCohan::logCallback(const std_msgs::String::ConstPtr& msg)
+void LogManagerCohan::odomRCB(const nav_msgs::Odometry::ConstPtr& msg)
 {
-	log_file_ << ros::Time::now() << " : " <<  msg->data << endl;
+	odom_r_ = *msg;
+
+	if(goal_received_ && !goal_done_)
+		log_file_ << "pose: " << msg->pose.pose.position.x << ", " << msg->pose.pose.position.y << ", " << msg->pose.pose.position.z << ", " << msg->pose.pose.orientation.w << ", vel: " << msg->twist.twist.linear.x << ", " << msg->twist.twist.linear.y << ", " << msg->twist.twist.angular.z << endl;
 }
 
-
-void LogManagerCohan::poseRCallback(const nav_msgs::Odometry::ConstPtr& msg)
+void LogManagerCohan::odomHCB(const nav_msgs::Odometry::ConstPtr& msg)
 {
-	log_file_ << ros::Time::now() << " : Pose: x= " << msg->pose.pose.position.x << ", y= " << msg->pose.pose.position.y << ","<< endl;
+	odom_h_ = *msg;
+}
+
+void LogManagerCohan::robotGoalCB(const geometry_msgs::PoseStamped::ConstPtr& msg)
+{
+	ROS_INFO("Goal started ! %d", id_);
+
+	log_file_.open(path_ + "/logs/log_data/cohan_logs/log_cohan" + std::to_string(id_) + ".txt");
+	log_file_ << "start: x= " << odom_r_.pose.pose.position.x << ", y= " << odom_r_.pose.pose.position.y << ", z= " << odom_r_.pose.pose.position.z << ", w= " << odom_r_.pose.pose.orientation.w << endl;
+	log_file_ << "goal: x= " << msg->pose.position.x << ", y= " << msg->pose.position.y << ", z= " << msg->pose.position.z << ", w= " << msg->pose.orientation.w << endl;
+
+	goal_received_ = true;
+	goal_done_ = false;
+}
+
+void LogManagerCohan::robotGoalStatusCB(const actionlib_msgs::GoalStatusArray::ConstPtr& msg)
+{
+	if(!msg->status_list.empty())
+	{
+		if(!goal_done_
+		&& ((msg->status_list.back().status == 0) 	// PENDING
+		|| (msg->status_list.back().status == 2) 		// PREEMPTED
+		|| (msg->status_list.back().status == 3) 		// SUCCEEDED
+		|| (msg->status_list.back().status == 4))) 	// ABORTED
+		{
+			ROS_INFO("Goal reached !");
+			log_file_.close();
+			goal_done_ = true;
+			goal_received_ = false;
+			id_++;
+		}
+	}
 }
 
 //////////////////////// MAIN ///////////////////////////////
 
 int main(int argc, char** argv)
 {
+	ROS_INFO("init");
+	
 	ros::init(argc, argv, "LogManagerCohan");
+
+	ROS_INFO("started");
 
 	LogManagerCohan log_cohan_manager;
 
